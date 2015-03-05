81bcbfbb2508fa3d590a7c330bc62554886446b1
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-03-05 17:25:09 -0300

    Comentários das funcionalidades do TAD asmWriter adicionados.

--- src/asmWriter/asmWriter.c
+++ src/asmWriter/asmWriter.c
@@ -1,6 +1,23 @@
-/* asmWriter.c
- * Implementaçao do TAD ASMWRITER.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  asmWriter.c
+ *
+ *       Descricao:  Implementação do TAD asmWriter.
+ *
+ *          Versao:  1.0
+ *       Criado em:  05/03/2015 17:11:43
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
+
 #include "asmWriter.h"
 
 ASMWRITER *asmWriter_new(const char *filename)
--- src/asmWriter/asmWriter.h
+++ src/asmWriter/asmWriter.h
@@ -1,6 +1,22 @@
-/* asmWriter.h
- * TAD do Buffer de Escrita de instrução em um arquivo binário.
+/*
+ * ===========================================================================
+ *
+ * Nome do Arquivo:  asmWriter.h
+ *
+ *       Descricao:  TAD para escrita do arquivo binário de saída do 
+ *       			 assembler.
+ *
+ *          Versao:  1.0
+ *       Criado em:  05/03/2015 17:14:00
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Gustavo Freitas de Amorim (), gustavofreitasamorim@gmail.com
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * ===========================================================================
  */
+
 
 #ifndef ASMWRITER_HEADER
 #define ASMWRITER_HEADER
@@ -12,9 +28,45 @@ typedef struct _asmWriter
     FILE *file;
 } ASMWRITER;
 
+
+/* -> ASMWRITER *     asmWriter_new(const char *filename)
+ * 
+ * - DESCRIÇÃO: Instancia um novo write do assembler que será utilizado na
+ *              escrita do arquivo binário.
+ *   
+ * - PARÂMETROS: 
+ *      -> const char *filename: Nome do arquivo a ser utilizado.
+ *
+ * - RETORNO: Estrutura do tipo ASMWRITER instanciada.
+ *   	-> Se NULL    - Erro na operação.
+ *   	-> Se != NULL - Sucesso na operação.
+ */
 ASMWRITER *     asmWriter_new(const char *filename);
+
+
+/* -> void asmWriter_free(ASMWRITER *asmWriter)
+ * 
+ * - DESCRIÇÃO: Destrói uma estrutura existente em memória.
+ *
+ * - PARÂMETROS: 
+ *      -> ASMWRITER *asmWriter: Estrutura a ser destruída.
+ *
+ * - RETORNO: void.
+ */
 void            asmWriter_free(ASMWRITER *asmWriter);
 
+
+/* -> void asmLoader_getNextInst(ASMLOADER *asmLoader)
+ * 
+ * - DESCRIÇÃO: Grava no arquivo a instrução binária recebida como parâmetro.
+ *
+ * - PARÂMETROS: 
+ *      -> ASMWRITER *asmWriter: Estrutura a ser utilizada para a escrita da 
+ *      instrução.
+ *      -> INSTRUCTION *instruction: Instrução a ser escrita.
+ *
+ * - RETORNO: void.
+ */
 void            asmWriter_writeInst(ASMWRITER *asmWriter
                                     /*, INSTRUCTION *instruction*/);
 
