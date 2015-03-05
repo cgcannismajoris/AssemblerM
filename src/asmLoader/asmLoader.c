07e2a64033e07716ce7cc66db1d0f35540227e49
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-03-05 17:01:09 -0300

    Comentários das funcionalidades do TAD asmLoader adicionados.

--- src/asmLoader/asmLoader.c
+++ src/asmLoader/asmLoader.c
@@ -1,7 +1,24 @@
-/* asmLoader.c
- * Implementaçao do TAD ASMLOADER.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  asmLoader.c
+ *
+ *       Descricao:  Implementação do TAD asmLoader.
+ *
+ *          Versao:  1.0
+ *       Criado em:  05/03/2015 16:57:11
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
 #include "asmLoader.h"
+
 
 ASMLOADER *asmLoader_new(const char *filename)
 {
@@ -14,7 +31,7 @@ void asmLoader_free(ASMLOADER *asmLoader)
 
 }
 
-void asmLoader_getNextInst(ASMLOADER *asmLoader)
+char *asmLoader_getNextInst(ASMLOADER *asmLoader)
 {
 
 }
--- src/asmLoader/asmLoader.h
+++ src/asmLoader/asmLoader.h
@@ -1,6 +1,23 @@
-/* asmLoader.h
- * TAD do Buffer de Leitura de instrução de um arquivo de texto de entrada.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  asmLoader.h
+ *
+ *       Descricao:  TAD para leitura do arquivo contendo o código fonte 
+ *       			 escrito pelo programador.
+ *
+ *          Versao:  1.0
+ *       Criado em:  05/03/2015 16:52:48
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
 #ifndef ASMLOADER_HEADER
 #define ASMLOADER_HEADER
 
@@ -8,11 +25,53 @@
 
 typedef struct _asmLoader
 {
-    FILE *file;
+    FILE *file; //Ponteiro para o arquivo a ser lido.
 } ASMLOADER;
 
+
+/* -> ASMLOADER *asmLoader_new(const char *filename)
+ * 
+ * - DESCRIÇÃO: Instancia um novo loader do assembler que será utilizado na leitura
+ *   de instruções escritas pelo programador.
+ *
+ * - PARÂMETROS: 
+ *      -> const char *filename: Nome do arquivo a ser utilizado.
+ *
+ * - RETORNO: Estrutura do tipo ASMLOADER instanciada.
+ *   	-> Se NULL    - Erro na operação.
+ *   	-> Se != NULL - Sucesso na operação.
+ */
 ASMLOADER *     asmLoader_new(const char *filename);
+
+
+/* -> void asmLoader_free(ASMLOADER *asmLoader)
+ * 
+ * - DESCRIÇÃO: Destrói uma estrutura existente em memória.
+ *
+ * - PARÂMETROS: 
+ *      -> ASMLOADER *asmLoader: Estrutura a ser destruída.
+ *
+ * - RETORNO: void.
+ */
 void            asmLoader_free(ASMLOADER *asmLoader);
-void            asmLoader_getNextInst(ASMLOADER *asmLoader);
+
+
+/* -> void asmLoader_getNextInst(ASMLOADER *asmLoader)
+ * 
+ * - DESCRIÇÃO: Carrega a próxima instrução escrita pelo programador presente no 
+ *   arquivo.
+ *   	- Se nenhuma intrução foi lida até o momento, carrega a primeira.
+ *   	- Se ja leu alguma, carrega a intrução subsequênte à anteriormente carregada.
+ *
+ * - PARÂMETROS: 
+ *      -> ASMLOADER *asmLoader: Estrutura a ser utilizada para a leitura da 
+ *      instrução.
+ *
+ * - RETORNO: Instrução escrita pelo programador carregada.
+ *   	-> Se NULL    - Erro na operação.
+ *   	-> Se != NULL - Sucesso na operação.
+ */
+char *         asmLoader_getNextInst(ASMLOADER *asmLoader);
+
 
 #endif /* ASMLOADER_HEADER */
