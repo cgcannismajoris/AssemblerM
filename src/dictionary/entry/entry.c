a2375e056e53f3849cee75dd2149edbd3b5e4993
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-03-06 21:08:09 -0300

    Implementação do ENTRY.

--- src/dictionary/entry/entry.c
+++ src/dictionary/entry/entry.c
@@ -1,24 +1,63 @@
 /* entry.c
  * Implementaçao do TAD ENTRY.
  */
+
 #include "entry.h"
 
-ENTRY *entry_new(const char *rotulo, int opcode)
+ENTRY *entry_new(char *rotulo, int opcode)
 {
-    return NULL;
+	
+	ENTRY *new = NULL;
+	char *new_rot = NULL;
+
+	if(rotulo != NULL && strlen(rotulo) <= 10){
+
+		new = (ENTRY*)malloc(sizeof(ENTRY));
+		new_rot = (char*)malloc(ENTRY_ROT_MAXLENGTH);
+
+		if(new != NULL) 
+		{
+			if(new_rot != NULL)
+			{
+				new->rotulo = new_rot;	
+				strncpy(new->rotulo, rotulo, ENTRY_ROT_MAXLENGTH);
+				new->opcode = opcode;
+			}
+			else
+			{ //new foi alocado mas o rótulo não
+				free(new);
+				new = ENTRY_EALLOC;
+			}
+		}
+	}
+
+    return new;
 }
 
 void entry_free(ENTRY *entry)
 {
 
+	free(entry->rotulo);
+	free(entry);	
+
 }
 
 const char *entry_getRotulo(ENTRY *entry)
 {
+	if(entry != NULL)
+	{
+		return(entry->rotulo);
+	}
 
+	return(NULL);
 }
 
-const char *entry_getOpcode(ENTRY *entry)
+uint8_t entry_getOpcode(ENTRY *entry)
 {
+	if(entry != NULL)
+	{
+		return(entry->opcode);
+	}
 
+	return(0);
 }
--- src/dictionary/entry/entry.h
+++ src/dictionary/entry/entry.h
@@ -5,17 +5,30 @@
 #define ENTRY_HEADER
 
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+ 
+#define ENTRY_EALLOC		NULL
+
+#define ENTRY_EALLOC_MSG 	"Falha na alocação da estrutura ASMLOADER."
+
+
+#define ENTRY_ROTCOD_ASCII 		1
+#define ENTRY_ROT_MAXLENGTH 	(11 * ENTRY_ROTCOD_ASCII)
+
 
 typedef struct _entry
 {
-    char *  rotuloDaInst;
-    int     opcode;
+    char *  rotulo;
+    uint8_t opcode;
 } ENTRY;
 
-ENTRY *         entry_new(const char *rotulo, int opcode);
+ENTRY *         entry_new(char *rotulo, int opcode);
 void            entry_free(ENTRY *entry);
 
 const char *    entry_getRotulo(ENTRY *entry);
-const char *    entry_getOpcode(ENTRY *entry);
+uint8_t         entry_getOpcode(ENTRY *entry);
 
 #endif /* ENTRY_HEADER */
