8ffa25bfb08d303ed2be443bb631910a5244127b
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-03-07 18:45:26 -0300

    Implementação do TAD DICWRITER.

--- src/dictionary/dicWriter/dicWriter.c
+++ src/dictionary/dicWriter/dicWriter.c
@@ -1,26 +1,87 @@
-/* dicWriter.c
- * Implementaçao do TAD DICWRITER.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  dicWriter.c
+ *
+ *       Descricao:  Implementação do TAD DICWRITER.
+ *
+ *          Versao:  1.0
+ *       Criado em:  07/03/2015 17:52:42
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
 #include "dicWriter.h"
 
 DICWRITER *dicWriter_new(const char *filename)
 {
-    return NULL;
+
+	DICWRITER *new = NULL;
+	FILE *file;
+
+	file = fopen(filename, "wb+");
+
+	if(file != NULL)
+	{
+
+		new = (DICWRITER*)malloc(sizeof(DICWRITER));
+		new->file = file;
+
+	}
+
+    return new;
 }
 
 void dicWriter_free(DICWRITER *dicWriter)
 {
-
+	if(dicWriter != NULL)
+	{
+		fclose(dicWriter->file);
+		free(dicWriter);
+	}
 }
 
-void dicWriter_writeQtdInst(DICWRITER *dicWriter, uint32_t num)
+int dicWriter_writeQtdInst(DICWRITER *dicWriter, uint64_t num)
 {
+	
+	if(dicWriter != NULL)
+	{
+		//Grava a quantidade de instruções armazenadas no arquivo
+		fwrite(&num, sizeof(uint64_t), 1, dicWriter->file);
 
+		return(0);
+	}
+	
+	return(DICWRITER_ENULLPOINTER);
 }
 
-void dicWriter_writeInst(DICWRITER *dicWriter,
-                                    const char *rotulo,
-                                    uint32_t opcode)
+int dicWriter_writeInst(DICWRITER *dicWriter, 
+							const char *instructionPattern,
+                          		uint32_t opcode)
 {
 
+	if(dicWriter != NULL && instructionPattern != NULL)
+	{
+		//Grava o padrão da instrução
+		fwrite(instructionPattern, sizeof(char), strlen(instructionPattern), 
+						dicWriter->file);
+
+		//Insere uma separação
+		fwrite(" ", sizeof(char), 1, dicWriter->file);
+
+		//Grava o opcode correspondente à instrução
+		fwrite(&opcode, sizeof(uint8_t), 1, dicWriter->file);
+
+		//Grava o terminador de instrução
+		fwrite("\0", sizeof(char), 1, dicWriter->file);
+
+		return(0);
+	}
+	
+	return(DICWRITER_ENULLPOINTER);
 }
--- src/dictionary/dicWriter/dicWriter.h
+++ src/dictionary/dicWriter/dicWriter.h
@@ -1,24 +1,108 @@
-/* dicWriter.h
- * TAD DICWRITER.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  dicWriter.h
+ *
+ *       Descricao:  TAD para a escrita do arquivo de padrões e opcodes
+ *                   aceitos pelo assembler.
+ *
+ *          Versao:  1.0
+ *       Criado em:  07/03/2015 17:50:06
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
 #ifndef DICWRITER_HEADER
 #define DICWRITER_HEADER
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DICWRITER_SUCCESS        0      /* Sucesso na operação (não vale para ponteiros) */
+#define DICWRITER_EWRITE        -1      /* Falha ao escrever no arquivo */
+#define DICWRITER_ENULLPOINTER  -2      /* Ponteiro nulo recebido */
+#define DICWRITER_EALLOC		NULL    /* FALHA DE ALOCAÇÃO */
+
+
+#define DICWRITER_EWRITE_MSG        "Falha ao escrever no arquivo em DICWRITER."
+#define DICWRITER_ENULLPOINTER_MSG  "Ponteiro nulo recebido em DICWRITER."
+#define DICWRITER_EALLOC_MSG        "Falha na alocação da estrutura DICWRITER."
+
 
 typedef struct _dicWriter
 {
-    FILE *file;
+    FILE *file; //Ponteiro para o arquivo utilizado nas operações de escrita.
 } DICWRITER;
 
+
+/* -> DICWRITER *dicWriter_new(const char *filename)
+ * 
+ * - DESCRIÇÃO: Instancia um novo writer do assembler que será utilizado na
+ *              escrita do arquivo de traduções da linguagem.
+ *   
+ * - PARÂMETROS: 
+ *      -> const char *filename: Nome do arquivo a ser utilizado.
+ *
+ * - RETORNO: Estrutura do tipo DICWRITER instanciada.
+ *   	-> Se NULL    - Erro na operação.
+ *   	-> Se != NULL - Sucesso na operação.
+ */
 DICWRITER *     dicWriter_new(const char *filename);
+
+
+/* -> void dicWriter_free(DICWRITER *dicWriter)
+ * 
+ * - DESCRIÇÃO: Destrói uma estrutura existente em memória.
+ *
+ * - PARÂMETROS: 
+ *      -> DICWRITER *dicWriter: Estrutura a ser destruída.
+ *
+ * - RETORNO: void.
+ */
 void            dicWriter_free(DICWRITER *dicWriter);
 
-void            dicWriter_writeQtdInst(DICWRITER *dicWriter, uint32_t num);
 
-void            dicWriter_writeInst(DICWRITER *dicWriter,
-                                    const char *rotulo,
-                                    uint32_t opcode);
+/* -> void dicWriter_writeQtdInst(DICWRITER *dicWriter, uint64_t num)
+ * 
+ * - DESCRIÇÃO: Grava no arquivo a instrução binária recebida como parâmetro.
+ *
+ * - PARÂMETROS: 
+ *      -> DICWRITER *dicWriter: Estrutura a ser utilizada para a escrita da 
+ *      quantidade de instruções.
+ *      -> uint64_t num: Quantidade de instruções a ser escrita.
+ *
+ * - RETORNO: Estado da operação.
+ *      -> Se 0                      - Sucesso na operação.
+ *      -> Se DICWRITER_ENULLPOINTER - Erro na operação.
+ */
+int             dicWriter_writeQtdInst(DICWRITER *dicWriter, uint64_t num);
+
+
+/* -> void dicWriter_writeInst(DICWRITER *dicWriter, const char *instructionPattern, 
+ *                               uint32_t opcode)
+ * 
+ * - DESCRIÇÃO: Grava no arquivo o padrão de instrução recebida como parâmetro.
+ *
+ * - PARÂMETROS: 
+ *      -> ASMWRITER *asmWriter: Estrutura a ser utilizada para a escrita da 
+ *      instrução.
+ *      -> const char *instructionPattern: Padrão de instrução a ser escrito.
+ *      -> uint32_t opcode: Opcode correspondente ao padrão recebido.
+ *
+ * - RETORNO: Estado da operação.
+ *      -> Se 0                      - Sucesso na operação.
+ *      -> Se DICWRITER_ENULLPOINTER - Erro na peração.
+ */
+int             dicWriter_writeInst(DICWRITER *dicWriter, 
+										const char *instructionPattern,
+                                    		uint32_t opcode);
 
 #endif /* DICWRITER_HEADER */
