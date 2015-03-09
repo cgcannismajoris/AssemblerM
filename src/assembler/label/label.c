fcfb91ee541a427e7ac768ef07fac6825b3ea136
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-03-09 18:39:41 -0300

    Implementado o TAD LABEL.

--- src/assembler/label/label.c
+++ src/assembler/label/label.c
@@ -1,24 +1,79 @@
-/* label.c
- * Implementação do TAD LABEL.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  label.c
+ *
+ *       Descricao:  Implementação do TAD LABEL.
+ *
+ *          Versao:  1.0
+ *       Criado em:  09/03/2015 18:37:48
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Gustavo Freitas de Amorim (), gustavofreitasamorim@gmail.com
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
 #include "label.h"
 
 LABEL *label_new(const char *name, uint64_t num)
 {
-    return NULL;
+	LABEL *new;
+
+	if(name != NULL){
+		return (NULL);
+	}
+
+	new = (LABEL*)malloc(sizeof(LABEL));
+
+	if(new != NULL){
+		
+		new->labelName = (char*)malloc(sizeof(char)*(strlen(name) + 1));
+
+		if(new->labelName != NULL){
+			strncpy(new->labelName, name, strlen(name));
+			new->labelLineNum = num;
+			new->next = NULL;
+		}
+		else{
+			free(new);
+			new = LABEL_EALLOC;
+		}
+	}
+
+    return new;
 }
 
 void label_free(LABEL *label)
 {
-
+	if(label != NULL){
+		free(label->labelName);
+		free(label);
+	}
 }
 
 char *label_getName(LABEL *label)
 {
-
+	if(label != NULL){
+		return (label->labelName);
+	}
+	return (NULL);
 }
 
 uint64_t label_getLineNum(LABEL *label)
 {
-    return 0;
+    if(label != NULL){
+		return (label->labelLineNum);
+	}
+
+	return (0);
+}
+
+void label_setNext(LABEL *label, LABEL *next){
+	if(label != NULL){
+		label->next = next;
+	}
 }
--- src/assembler/label/label.h
+++ src/assembler/label/label.h
@@ -1,19 +1,40 @@
-/* label.h
- * TAD LABEL.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  label.h
+ *
+ *       Descricao:  TAD de lista encadeada simples de labels para auxíliar o
+ *       			 assembler na indexação de labels.
+ *
+ *          Versao:  1.0
+ *       Criado em:  09/03/2015 18:38:19
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Gustavo Freitas de Amorim (), gustavofreitasamorim@gmail.com
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
 #ifndef LABEL_HEADER
 #define LABEL_HEADER
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+
 
 #define LABEL_LABELNAMELENGTH   20
 
+#define LABEL_EALLOC			NULL
+
+#define LABEL_STR_EALLOC		"Falha na alocação de memória no TAD LABEL."
 
 typedef struct _label
 {
-    char *          labelName[LABEL_LABELNAMELENGTH];
+    char *          labelName;
     uint64_t        labelLineNum;
     struct _label * next;
 } LABEL;
