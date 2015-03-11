b4a366dc894c156a67f3fbdb17057f89015c4e44
Gustavo Freitas de Amorim <gustavofreitasamorim@gmail.com>
2015-03-11 18:58:17 -0300

    Implementado o TAD TOKENS.

--- src/scanner/tokens/tokens.c
+++ src/scanner/tokens/tokens.c
@@ -1,29 +1,95 @@
-/* tokens.h
- * Implementação do TAD TOKENS.
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  tokens.c
+ *
+ *       Descricao:  Implementação do TAD TOKENS.
+ *
+ *          Versao:  1.0
+ *       Criado em:  11/03/2015 14:55:56
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
 #include "tokens.h"
 
 TOKENS *token_new(uint8_t qtd)
 {
-    return NULL;
+	
+	TOKENS *new = NULL;
+	
+	new = (TOKENS*)malloc(sizeof(TOKENS));
+	
+	if(new != NULL){
+
+		new->tokens = (char**)malloc(sizeof(char*) * qtd);
+		
+		if(new->tokens != NULL){
+			memset(new->tokens, 0, qtd);
+			new->qtdUsed = 0;
+			new->qtdMax = qtd;
+		}
+		else{
+			free(new);
+			new = TOKENS_EALLOC;
+		}
+	}
+
+	return new;
 }
 
 void token_free(TOKENS *token)
 {
+	
+	uint8_t qtd;
 
+	if(token != NULL){
+		
+		//Destrói as instrings
+		for(qtd = 0; qtd < token->qtdMax; qtd++){
+			free(token->tokens[qtd]);
+		}
+
+		free(token->tokens);
+		free(token);
+
+	}
 }
 
 uint8_t token_getQtd(TOKENS *token)
 {
+
+	if(token != NULL){
+		return (token->qtdUsed);
+	}
+
     return 0;
 }
 
 char *token_getToken(TOKENS *token, uint8_t pos)
 {
+
+	if(token != NULL && pos < token->qtdMax){
+		return (token->tokens[pos]);
+	}
+
     return NULL;
 }
 
-void token_setToken(TOKENS *token, char *t, uint8_t pos)
-{
-
+void token_addToken(TOKENS *token, char *t){
+	
+	if(token != NULL && t != NULL && token->qtdUsed < token->qtdMax){
+		token->tokens[token->qtdUsed] = (char*)malloc(sizeof(char) * strlen(t));
+		memcpy(token->tokens[token->qtdUsed], t, strlen(t));
+		
+		token->qtdUsed++;
+	}
 }
+
+
--- src/scanner/tokens/tokens.h
+++ src/scanner/tokens/tokens.h
@@ -1,28 +1,113 @@
-/* tokens.h
- * TAD TOKENS (Conjunto de TOKENS).
+/*
+ * ==========================================================================
+ *
+ * Nome do Arquivo:  tokens.h
+ *
+ *       Descricao:  TAD para armazenar os tokens detectados pelo
+ *       			 scaner do assembler.
+ *
+ *          Versao:  1.0
+ *       Criado em:  11/03/2015 14:57:04
+ *         Revisao:  none
+ *      Compilador:  gcc
+ *
+ *           Autor:  Cristian Costa Mello, Gustavo Freitas de Amorim
+ *     Organizacao:  VY Cannis Majoris
+ *
+ * =========================================================================
  */
+
+
+
 #ifndef TOKENS_HEADER
 #define TOKENS_HEADER
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 
-#define TOKENS_QTDMAX   256
 
+#define TOKENS_EALLOC       NULL
+
+#define TOKENS_EALLOC_MSG   "Falha ao alocar memória no TAD TOKENS."
+
+ 
 typedef struct _tokens
 {
-    char **  tokens;
-    uint8_t  qtdUsed;
+    char **  tokens;    //Vetor onde são armazenados os tokens
+    uint8_t  qtdUsed;   //Quantidade de tokens armazenados
+	uint8_t  qtdMax;    //Quantidade máxima de tokes (tamanho do vetor char **tokens)
 } TOKENS;
 
 
+/* -> TOKENS *token_new(uint8_t qtd)
+ * 
+ * - DESCRIÇÃO: Instancia uma nova estrutura do tipo TOKENS para armazenar os 
+ *   			tokens gerados pelo scanner.   
+ *
+ * - PARÂMETROS: 
+ *      -> uint8_t qtd: Quantidade máxima de tokens que poderão ser armazenados
+ *      				na estrutura.
+ *
+ * - RETORNO: Estrutura do tipo TOKENS instanciada.
+ *   	-> Se NULL    - Erro na operação.
+ *   	-> Se != NULL - Sucesso na operação.
+ */
 TOKENS *    token_new(uint8_t qtd);
+
+
+/* -> void token_free(TOKENS *token)
+ *
+ * - DESCRIÇÃO: Destrói uma estrutura existente em memória.
+ *
+ * - PARÂMETROS: 
+ *      -> TOKENS *token: Estrutura a ser destruída.
+ *
+ * - RETORNO: void.
+ */
 void        token_free(TOKENS *token);
 
-uint8_t     token_getQtd(TOKENS *token);
-char *      token_getToken(TOKENS *token, uint8_t pos);
-void        token_setToken(TOKENS *token, char *t, uint8_t pos);
 
+/* -> uint8_t token_getQtd(TOKENS *token)
+ * 
+ * - DESCRIÇÃO: Retorna a quantidade de tokens armazenados na estrutura recebida.
+ *
+ * - PARÂMETROS: 
+ *      -> TOKENS *token: Estrutura a ser utilizada na operação.
+ *
+ * - RETORNO: Quantidade de tokens armazenados.
+ *   	-> Se 0    - Erro na operação.
+ *   	-> Se != 0 - Sucesso na operação.
+ */
+uint8_t     token_getQtd(TOKENS *token);
+
+
+/* -> char *token_getToken(TOKENS *token, uint8_t pos)
+ * 
+ * - DESCRIÇÃO: Retorna o token armazenado na estrutura em uma determinada posição.
+ *
+ * - PARÂMETROS: 
+ *      -> TOKENS *token: Estrutura a ser utilizada na operação.
+ *      -> uint8_t pos: Posição do token desejado.
+ *
+ * - RETORNO: Token armazenado na posição desejada.
+ *   	-> Se NULL    - Erro na operação.
+ *   	-> Se != NULL - Sucesso na operação.
+ */
+char *      token_getToken(TOKENS *token, uint8_t pos);
+
+
+/* -> void token_addToken(TOKENS *token, char *t)
+ * 
+ * - DESCRIÇÃO: Adiciona um token na primeira posição disponível.
+ *
+ * - PARÂMETROS: 
+ *      -> TOKENS *token: Estrutura a ter o token adicionado.
+ *      -> char *t: Token a ser armazenado.
+ *
+ * - RETORNO: void.
+ */
+void        token_addToken(TOKENS *token, char *t);
 
 #endif /* TOKENS_HEADER */
